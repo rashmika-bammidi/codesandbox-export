eb877a23ee9f45fabbfff7b5b826678e53ec2ea9
rashmika-bammidi <131141273+rashmika-bammidi@users.noreply.github.com>
2023-04-18 23:16:40 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const react = require("react");
+function Footer() {
+  const currYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright@{currYear}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,13 @@
+const react = require("react");
+function Header() {
+  return (
+    <div className="note">
+      <h1> React.js Bootcamp</h1>
+      <p>
+        We learnt the basics of frontend web-development,javaScript,JSX,React,
+        ReactDOM and created a project at the very end of the bootcamp as well .
+      </p>
+    </div>
+  );
+}
+export default Header;
